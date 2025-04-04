3cd466d3dee3ab67d6351327a5af1534a9fad7cd
Jbnye <jbnye@umich.edu>
2025-04-04 04:11:49 -0400

    easy, binary add fix, palindrome fix, and remove linked list duplicates

--- Easy/09_Palindrome_Number.py
+++ Easy/09_Palindrome_Number.py
@@ -1,21 +1,34 @@
 class Solution(object):
     def isPalindrome(self, x):
-        array = []
-        y = x
-        if(x < 0):
+        if(x<0):
             return False
-        while y != 0:
-            array.append(y%10)
-            y = (y / 10)
-        left = 0
-        right = len(array) - 1
-
-        while left <= right:
-            if(array[left] == array[right]):
-                left += 1
-                right -= 1
-            else:
-                return False
-        return True
+        num = 0
+        y = x
+        while(y != 0):
+            num = (num * 10) + (y%10)
+            y = y // 10
+        return (x == num)
         
-print (3 // 2)
+print (3 // 2)    
+
+
+
+
+# def isPalindrome(self, x):
+    #     array = []
+    #     y = x
+    #     if(x < 0):
+    #         return False
+    #     while y != 0:
+    #         array.append(y%10)
+    #         y = (y / 10)
+    #     left = 0
+    #     right = len(array) - 1
+
+    #     while left <= right:
+    #         if(array[left] == array[right]):
+    #             left += 1
+    #             right -= 1
+    #         else:
+    #             return False
+    #     return True
